40e230f37f8a74c8331078030a515b76edc6345f
Ivan Surzhytski <8717619@gmail.com>
2020-04-30 17:12:02 +0300

    homework 1.3

---
+++ homework - 1/homework 1.3.py
@@ -0,0 +1,13 @@
+first_list = []
+second_list = []
+x = 0
+N = int(input('Enter number N (n>2): '))
+for number in range(N):
+    first_list.append(int(input('Enter number from this list: ')))
+print(f"First list: {first_list}")
+for number in first_list:
+    if number == x:
+        second_list.insert(0, number)
+    if number > x or number < x:
+        second_list.append(number)
+print(f"\nYour right list is: {second_list}  Congratulations!")
