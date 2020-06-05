1e066557618924a03a874905d3c2d884d94970cf
Ivan Surzhytski <8717619@gmail.com>
2020-06-05 15:09:20 +0300

    hw5 - task 1

--- hw5/1.descriptor- readonly.py
+++ hw5/1.descriptor- readonly.py
@@ -1,12 +1,19 @@
 class Readonly(object):
-    def __init__(self, change):
-        self.change = change
+    def __init__(self, fget=None, fset=None):
+        self.fget = fget
+        self.fset = fset
 
-    def __get__(self, instance, owner):
-        return self.change(instance)
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError
+        return self.fget(obj)
 
-    def __set__(self, instance, value):
-        return self.change(value)
+    def __set__(self, obj, value):
+        if self.fset is None:
+            raise AttributeError
+        self.fset(obj, value)
 
 
 class Test(object):
@@ -22,6 +29,12 @@ test = Test()
 print(test.val)  # OK, выводит 1
 test.val = 2  # AttributeError
 
-
-
-
+# class Readonly(object):
+#     def __init__(self, change):
+#         self.change = change
+#
+#     def __get__(self, instance, owner):
+#         return self.change(instance)
+#
+#     def __set__(self, instance, value):
+#         return self.change(value)
